04fd1e98ab3f17a73b66fe06d4cf21a7fdd413b4
Jasper-Williams <jasper.williams@ascte.org>
2026-08-17 20:47:55 -0500

    Added in a quick header for the Dashboard Page
    Just wanted to have something down in case we accidently went to this page. I wanted the developer to be able to know which page their on very easily.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Text } from "@chakra-ui/react";
+
+export default function DashboardPage() {
+  return (
+    <Text textAlign="center" fontSize="5xl" color="fg.muted">
+      Dashboard
+    </Text>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Montserrat} from "next/font/google";
 import "./globals.css";
 import { Provider } from "@/components/ui/provider";
 
-const geistSans = Geist({
+const geistSans = Montserrat({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Montserrat({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
